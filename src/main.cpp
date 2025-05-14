2a0477ac083563573935ce191e865b43742261b4
idiestro <nachodiestrogil@gmail.com>
2025-05-14 16:36:55 +0200

    Update main - Get simulated data, save into payload and mqtt send

--- src/main.cpp
+++ src/main.cpp
@@ -7,40 +7,19 @@
 #include "config/mainConfig.h"
 
 //Utils libraries
-#include "SimulatedMeasures.h"
 #include "MqttConnection.h"
 #include "LogsUtils.h"
-#include "JsonUtils.h"
+#include "MeasuresDataHelper.h"
 
 //Instance classes
-SimulatedMeasures simulatedMeasures;
 MqttConnection mqttConnection;
-JsonUtils jsonUtils;
+MeasuresDataHelper measuresDataHelper;
+
+//Main values
+String composterId;
 
 
 void setup(void) {
-    //Init M5 and serial port
-    init();
-}
-
-void loop(void) {
-    // Initial CPU pause
-    delay(100);
-
-    //JOB TEST
-    jsonUtils.loadDefaultJson();
-    jsonUtils.setValue("m5BateryLevel", String(simulatedMeasures.getEnvironmentTemperature()), "float");
-
-    Serial.println(jsonUtils.getJsonAsString());
-
-    //Final delay
-    delay(2000);
-
-
-}
-
-//Initial system load and config
-void init() {
     //Init M5 and serial port
     auto cfg = M5.config();
     M5.begin(cfg);
@@ -50,7 +29,29 @@ void init() {
     //Init wifi and broker connection
     mqttConnection.init();
     LogsUtils::printLog("---Sistema iniciado---");
+    //Get composter ID
+    composterId = mqttConnection.getClientId();
     //Wait after setup
     delay(5000);
+}
+
+void loop(void) {
+    // Initial CPU pause
+    delay(10);
+
+    //Mqtt Connection loop
+    mqttConnection.loop(); 
+
+    // Read values and send messages
+    static unsigned long lastPublishTime = 0;
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - lastPublishTime >= 10000) { // 10 seconds
+        lastPublishTime = currentMillis;
+            // Set composter ID
+            measuresDataHelper.setComposterId(composterId);
+            // Set measures data
+            mqttConnection.topicPublication(measuresDataHelper.setMeasuresIntoJson());
+    }
 }
 
